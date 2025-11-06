9ff9f8fbee60b962faefc97ce0f114a6069e6168
Robert-gut <71551473+Robert-gut@users.noreply.github.com>
2025-11-06 19:25:05 +0200

    modules

---
+++ JS/28 JS (mudules)/js/main.js
@@ -0,0 +1,20 @@
+// import plus from './modules/plus.js'
+// import minus from './modules/minus.js';
+// import multiplication from './modules/multiplication.js';
+// import division from './modules/division.js';
+
+// import { plus, minus, division, multiplication } from './modules/math.js'
+
+
+// console.log(plus(5,2));
+// console.log(minus(10,2));
+// console.log(division(10,2));
+// console.log(multiplication(5,2));
+
+
+import math from "./modules/math.js";
+
+console.log(math.plus(5,2));
+console.log(math.minus(10,2));
+console.log(math.division(10,2));
+console.log(math.multiplication(5,2));
---
+++ JS/28 JS (mudules)/js/modules/math.js
@@ -0,0 +1,34 @@
+export function plus(num1, num2) {
+  return num1 + num2
+}
+
+export function minus(num1, num2) {
+  return num1 - num2
+}
+
+export function multiplication(a,b){
+  return a * b
+}
+
+export function division(num1, num2) {
+  return num1 / num2
+}
+
+
+const math = {
+  plus: function(num1, num2) {
+    return num1 + num2
+  },
+  minus: function(num1, num2) {
+  return num1 - num2
+  },
+  multiplication: function(a,b){
+    return a * b
+  },
+  division: function(num1, num2) {
+  return num1 / num2
+  }
+}
+
+
+export default math
