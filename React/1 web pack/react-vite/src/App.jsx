582f6487c6894a211ac30e521a36ae45b0482c0f
Robert-gut <71551473+Robert-gut@users.noreply.github.com>
2025-11-20 21:02:36 +0200

    vire

---
+++ React/1 web pack/react-vite/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import Header from './Components/Header/Header'
+import Main from './Components/Main/Main'
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Header/>
+      <Header/>
+      <Header/>
+      <Header/>
+      <h1 className='color'>hello world</h1>
+      <Main/>
+      <h2>test</h2>
+    </>
+  )
+}
+
+
+export default App
